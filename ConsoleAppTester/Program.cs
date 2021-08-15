5d0306772c6139ce6540bfc8106ee60c37bd4152
orlhatundji <orlhatundji@gmail.com>
2021-08-15 19:03:40 +0100

    added parsers for html and ppt

--- ConsoleAppTester/Program.cs
+++ ConsoleAppTester/Program.cs
@@ -8,12 +8,25 @@ namespace ConsoleAppTester
     {
         static void Main(string[] args)
         {
-            string folderName = AppDomain.CurrentDomain.BaseDirectory;
-            string pdfFilePath = Path.Combine(folderName, "..\\..\\..\\testPdf1.pdf");
+            //string folderName = AppDomain.CurrentDomain.BaseDirectory;
+            //string pdfFilePath = Path.Combine(folderName, "..\\..\\..\\testPdf1.pdf");
 
-            Console.WriteLine(pdfFilePath);
-            string text = PDFParser.Parse(pdfFilePath);
-            Console.WriteLine(text);
+            //Console.WriteLine(pdfFilePath);
+            //string text = PDFParser.Parse(pdfFilePath);
+            //Console.WriteLine(text);
+
+            //string path = @"C:/Users/Simeon/Desktop/index.html";
+            //string result = HTMLParser.parseHtml(path);
+            //Console.WriteLine(result);
+
+            //string path = @"C:/Users/Simeon/Desktop/QLAS proposal.docx";
+            //string result = DOCParser.parseDoc(path);
+            //Console.WriteLine(result);\
+
+            string path = @"C:/Users/Simeon/Desktop/textppt.pptx";
+            string result = PPTParser.parsePPT(path);
+            Console.WriteLine(result);
         }
     }
 }
+
