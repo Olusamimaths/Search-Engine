bc106fc24530b41a642f057b6e6f048277d0d986
orlhatundji <orlhatundji@gmail.com>
2021-08-21 22:36:05 +0100

    added parser for XLSX, XLS, CSV and TSV

---
+++ SearchEngine/src/Parsers/SpreadSheetParser.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+using IronXL;
+using Utilities;
+
+namespace Parsers
+{
+    /// <summary>
+    //Handles spreadsheet. Supported formats include: XLSX, XLS, CSV and TSV
+    /// </summary>
+    public static class SpreadSheetParser
+    {
+        /// <summary>
+        /// Parses a spreadsheet document and extracts the text content from it 
+        /// </summary>
+        /// <param name="filePath">String: File Path to the word document</param>
+        /// <returns>Text content of the spreadsheet</returns>
+        public static string parse(string filePath)
+        {
+            StringBuilder sb = new StringBuilder("");
+
+            try
+            {
+                WorkBook workbook = WorkBook.Load(filePath);
+                WorksheetsCollection worksheets = workbook.WorkSheets;
+
+                // Initialize String builder instance
+                foreach (var sheet in worksheets)
+                {
+                    foreach (var cell in sheet)
+                    {
+                        if (cell.Text.Length > 0)
+                        {
+                            sb.Append(cell.Text);
+                            sb.Append(" ");
+                        }
+                    }
+                }
+
+            }
+            catch (SystemException ex)
+            {
+                Logger.Info(ErrorMessages.ErrorOccurred);
+                Logger.Error(ex.Message);
+            }
+
+            
+            return sb.ToString();
+        }
+        
+
+}
+}
