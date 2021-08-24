738d99ba9e7a5cf469ff243f8065897bdb149b3a
Samuel Olusola <olusola.samuel.oluwatobi@gmail.com>
2021-08-24 17:27:51 +0100

    :zap: [FEAT]: Indexer

--- SearchEngine/src/Indexing/InvertedIndex.cs
+++ SearchEngine/src/Indexing/InvertedIndex.cs
@@ -2,9 +2,18 @@
 
 namespace Indexing
 {
+
+    /// <summary>
+    /// Creates an inverted index for documents
+    /// </summary>
     public class InvertedIndex
     {
         private readonly IDictionary<string, List<int>> _data = new Dictionary<string, List<int>>();
+        /// <summary>
+        /// Adds a entry to the inverted index data structure
+        /// </summary>
+        /// <param name="term">String: Term that will be the key of the etry</param>
+        /// <param name="documentId">int: Document unique Id</param>
         internal void Append(string term, int documentId)
         {
             if (_data.ContainsKey(term))
@@ -18,6 +27,11 @@ namespace Indexing
             }
         }
 
+        /// <summary>
+        /// Gets the number of times a term is present in the documents
+        /// </summary>
+        /// <param name="term">String: The term to compute the frequency for</param>
+        /// <returns>Int: number of times the term is present</returns>
         public int GetFrequency(string term)
         {
             return _data[term].Count;
