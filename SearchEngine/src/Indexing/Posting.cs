afc1de8e036f9a73096d096d9464dceecb2d77bd
Samuel Olusola <olusola.samuel.oluwatobi@gmail.com>
2021-08-24 09:45:54 +0100

    :construction: [WIP]: Indexer in progress

---
+++ SearchEngine/src/Indexing/Posting.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+
+namespace Indexing
+{
+    public class Posting
+    {
+        public int DocumentId { get; private set;  }
+
+        //public IList<long> Locations { get; } = new List<long>();
+
+        public Posting(int docID)
+        {
+            DocumentId = docID;
+        }
+    }
+}
