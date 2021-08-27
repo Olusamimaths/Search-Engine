40ff2684a3e021747cf3daecceae66f4dc960817
orlhatundji <orlhatundji@gmail.com>
2021-08-27 17:41:30 +0100

    added path handler class

--- SearchEngine/src/Utilities/PathHandler.cs
+++ SearchEngine/src/Utilities/PathHandler.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Utilities
 {
@@ -13,17 +10,12 @@ namespace Utilities
         {
             string appdomain = AppDomain.CurrentDomain.BaseDirectory;
             string newPath = Path.GetFullPath(Path.Combine(appdomain, @"..\..\..\"));
-            Logger.Info("appdomain" + appdomain);
-            Logger.Info("newPath" + newPath);
             return newPath;
         }
         public static string GetFilePath(string path)
         {
-            string appdomain = AppDomain.CurrentDomain.BaseDirectory;
-            string newPath = Path.GetFullPath(Path.Combine(appdomain, @"..\..\..\"));
-            Logger.Info("appdomain" + appdomain);
-            Logger.Info("newPath" + newPath);
-            return newPath;
+            string basePath = GetBasePath();
+            return basePath + path;
         }
     }
 }
