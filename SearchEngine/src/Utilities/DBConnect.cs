a866450479f912992cd58ab374454d2398c8874d
orlhatundji <orlhatundji@gmail.com>
2021-08-29 19:08:59 +0100

    integrate database

---
+++ SearchEngine/src/Utilities/DBConnect.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using MongoDB.Driver;
+
+namespace Utilities
+{
+    public class DBConnect
+    {
+        public static MongoClient Connect()
+        {
+            MongoClient dbClient = new MongoClient("mongodb+srv://pacesetter:pacesetter@cluster0.whodz.mongodb.net/test");
+            return dbClient;
+        }
+    }
+}
