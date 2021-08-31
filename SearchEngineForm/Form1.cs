2f3f3cd15ed66687d16a060ed356d189912c9f6b
Omolajacodec <Omolaja.abubakar05@gmail.com>
2021-08-31 11:37:20 +0100

    Recent update to form1

---
+++ SearchEngineForm/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SearchEngine.src.Uploader;
+namespace SearchEngineForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            
+            Application.Run(new Form1());
+        }
+    }
+   
+}
