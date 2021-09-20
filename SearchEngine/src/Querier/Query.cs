ec937b909d36886f713e207f9bf27bd44b7ad844
Samuel Olusola <olusola.samuel.oluwatobi@gmail.com>
2021-09-20 11:47:20 +0100

    WIP

--- SearchEngine/src/Querier/Query.cs
+++ SearchEngine/src/Querier/Query.cs
@@ -20,7 +20,7 @@ namespace SearchEngine
             private set;
         }
 
-        private List<List<Posting>> GetListOfPostingsForQuery(string query)
+        private static List<List<Posting>> GetListOfPostingsForQuery(string query)
         {
             List<List<Posting>> posting_list = new();
 
@@ -47,6 +47,64 @@ namespace SearchEngine
             }
             return posting_list;
         }
+
+        private static List<int> _Search(string query)
+        {
+            List<List<Posting>> listOfPostings = GetListOfPostingsForQuery(query);
+            List<int> matchedDocs = new();
+            foreach(List<Posting> postings in listOfPostings)
+            {
+                
+            }
+
+            return matchedDocs;
+        }
+
+        private static void SearchQueryOfTwoTerms(List<Posting> list1, List<Posting> list2) {
+            List<Posting> list = _TwoMerge(list1, list2);
+            List<int> documentIds = new();
+            foreach(Posting posting in list)
+            {
+                documentIds.Add(posting.DocumentID);
+            }
+
+
+
+        }
+        private static List<Posting> _TwoMerge(List<Posting> list1, List<Posting> list2)
+        {
+            List<Posting> list = new();
+
+            int current1 = 0;   // current index of list1
+            int current2 = 0;   // current index of list2
+            int current3 = 0;   // current index of list3
+
+            while (current1 < list1.Count && current2 < list2.Count)
+            {
+                if (list1[current1].DocumentID > list2[current2].DocumentID)
+                {
+                    list[current3++] = list2[current2++];
+                }
+                else
+                {
+                    list[current3++] = list1[current1++];
+                }
+            }
+
+            // copy the remaining items in list1 and list2 if any
+            while (current1 < list1.Count)
+            {
+                list[current3++] = list1[current1++];
+            }
+
+            while (current2 < list2.Count)
+            {
+                list[current3++] = list2[current2++];
+            }
+
+            return list;
+        }
+
         public static List<int> Search(string query)
         {
             List<int> matchedDocs = new();
